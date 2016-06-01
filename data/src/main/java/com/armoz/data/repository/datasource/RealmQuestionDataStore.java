83d0fe5006b566c8093d024004ce67e6f6ae4b4e
Rubèn Arana <ruarmo@hotmail.com>
2016-06-01 19:55:51 +0200

    Using initialData from realm to init database

--- data/src/main/java/com/armoz/data/repository/datasource/RealmQuestionDataStore.java
+++ data/src/main/java/com/armoz/data/repository/datasource/RealmQuestionDataStore.java
@@ -2,7 +2,16 @@ package com.armoz.data.repository.datasource;
 
 import android.content.Context;
 
+import com.armoz.data.R;
 import com.armoz.data.entity.QuestionEntity;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -10,8 +19,21 @@ import rx.Observable;
 
 public class RealmQuestionDataStore implements QuestionDataStore {
 
+    private Context context;
+
     public RealmQuestionDataStore(Context context) {
-        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).build();
+        this.context = context;
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(context).
+                initialData(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        String json = readJsonFromRawResource(R.raw.questions);
+                        Type type = new TypeToken<List<QuestionEntity>>() {}.getType();
+                        List<QuestionEntity> questionEntityList = new Gson().fromJson(json, type);
+                        realm.copyToRealm(questionEntityList);
+                    }
+                })
+                .build();
         Realm.setDefaultConfiguration(realmConfig);
     }
 
@@ -23,14 +45,28 @@ public class RealmQuestionDataStore implements QuestionDataStore {
 
     @Override
     public Observable<Boolean> initializeDatabase() {
+        /*String json = readJsonFromRawResource(R.raw.questions);
+        Type type = new TypeToken<List<QuestionEntity>>() {}.getType();
+        List<QuestionEntity> questionEntityList = new Gson().fromJson(json, type);
         Realm realm = Realm.getDefaultInstance();
         realm.beginTransaction();
-        QuestionEntity questionEntity = new QuestionEntity();
-        questionEntity.setId(1);
-        questionEntity.setQuestion("This is a question from RealmQuestionDataStore");
-        questionEntity.setAnswer("yes");
-        realm.copyToRealmOrUpdate(questionEntity);
-        realm.commitTransaction();
+        realm.copyToRealm(questionEntityList);
+        realm.commitTransaction();*/
         return Observable.just(true);
+    }
+
+    private String readJsonFromRawResource(int rawResourceId) {
+        try {
+            InputStream is = context.getResources().openRawResource(rawResourceId);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            return out.toString();
+        } catch (Exception e) {
+            throw new IllegalStateException("Error reading from raw fileId " + rawResourceId);
+        }
     }
 }
